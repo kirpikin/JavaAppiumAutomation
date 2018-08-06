81be5f30b271d6a3b51bd2a42f9bafc441d831e1
igor <kirpikin@gmail.com>
2018-08-06 21:28:11 +0300

    add HomeworkEight

--- src/HomeworkThree.java
+++ src/HomeworkThree.java
@@ -1,102 +1,20 @@
-import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.android.AndroidDriver;
-import org.junit.After;
-import org.junit.Before;
+import lib.CoreTestCase;
+import lib.ui.SearchPageObject;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.net.URL;
-
-public class HomeworkThree {
-
-    private AppiumDriver driver;
-
-    @Before
-    public void setUp() throws Exception {
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "AndroidTestDevice");
-        capabilities.setCapability("platform", "8.0");
-        capabilities.setCapability("automationName", "Appium");
-        capabilities.setCapability("appPackage", "org.wikipedia");
-        capabilities.setCapability("appActivity", ".main.MainActivity");
-        capabilities.setCapability("app", "/Users/igor/Desktop/JavaAppiumAutomation/apks/org.wikipedia.apk");
-
-        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
-    }
-
-    @After
-    public void tearDown() {
-        driver.quit();
-    }
+public class HomeworkThree extends CoreTestCase {
 
     @Test
     public void testCancelSearchResults() {
         //    Ex3: Тест: отмена поиска
 
-        waitForElementAndClick(
-                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
-                "Cannot find Search Wikipedia input",
-                5
-        );
+        SearchPageObject SearchPageObject = new SearchPageObject(driver);
+        SearchPageObject.initSearchInput();
+        SearchPageObject.typeSearchLine("Java");
+        SearchPageObject.waitForResultsListsPresent();
+        SearchPageObject.clickCancelSearch();
+        SearchPageObject.waitForResultsListsNotPresent();
 
-        waitForElementAndSendKeys(
-                By.xpath("//*[contains(@text, 'Search…')]"),
-                "Java",
-                "Cannot find search input",
-                5
-        );
-
-        waitForElementPresent(
-                By.id("org.wikipedia:id/search_results_list"),
-                "Cannot find results",
-                15
-        );
-
-        waitForElementAndClick(
-                By.id("org.wikipedia:id/search_close_btn"),
-                "Cannot find X to cancel search",
-                5
-        );
-
-        waitForElementNotPresent(
-                By.id("org.wikipedia:id/search_results_list"),
-                "We see results",
-                5
-        );
-
-    }
-
-
-    private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds) {
-        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
-        wait.withMessage(error_message + "\n");
-        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
-    }
-
-    private WebElement waitForElementAndClick(By by, String error_message, long timeoutInSeconds) {
-        WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
-        element.click();
-        return element;
-    }
-
-    private WebElement waitForElementAndSendKeys(By by, String value, String error_message, long timeoutInSeconds) {
-        WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
-        element.sendKeys(value);
-        return element;
-    }
-
-    private boolean waitForElementNotPresent(By by, String error_message, long timeoutInSeconds) {
-        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
-        wait.withMessage(error_message + "\n");
-
-        return wait.until(ExpectedConditions.invisibilityOfElementLocated(by));
     }
 
 }
--- src/tests/MyListsTest.java
+++ src/tests/MyListsTest.java
@@ -1,3 +1,5 @@
+package tests;
+
 import lib.CoreTestCase;
 import lib.ui.ArticlePageObject;
 import lib.ui.MyListsPageObject;
@@ -5,13 +7,9 @@ import lib.ui.NavigationUI;
 import lib.ui.SearchPageObject;
 import org.junit.Test;
 
-public class HomeworkFive extends CoreTestCase {
-
+public class MyListsTest extends CoreTestCase {
     @Test
-    public void testHomeworkFive() {
-        //    Ex5: Тест: сохранение двух статей
-
-        String name_of_folder = "Learning programming";
+    public void testSaveFirstArticleToMyList() {
 
         SearchPageObject SearchPageObject = new SearchPageObject(driver);
         SearchPageObject.initSearchInput();
@@ -21,14 +19,9 @@ public class HomeworkFive extends CoreTestCase {
         ArticlePageObject ArticlePageObject = new ArticlePageObject(driver);
         ArticlePageObject.waitForTitleElement();
 
+        String article_title = ArticlePageObject.getArticleTitle();
+        String name_of_folder = "Learning programming";
         ArticlePageObject.addArticleToMyList(name_of_folder);
-        ArticlePageObject.closeArticle();
-
-        SearchPageObject.initSearchInput();
-        SearchPageObject.typeSearchLine("Java");
-        SearchPageObject.clickByArticleWithSubstring("Programming language");
-        ArticlePageObject.waitForTitleElement();
-        ArticlePageObject.addArticleToExcitingList(name_of_folder);
         ArticlePageObject.closeArticle();
 
         NavigationUI NavigationUI = new NavigationUI(driver);
@@ -37,11 +30,7 @@ public class HomeworkFive extends CoreTestCase {
         MyListsPageObject MyListsPageObject = new MyListsPageObject(driver);
         MyListsPageObject.openFolderByName(name_of_folder);
 
-        MyListsPageObject.swipeByArticleToDelete("Java (programming language)");
+        MyListsPageObject.swipeByArticleToDelete(article_title);
 
-        MyListsPageObject.waitForArticleToDissappearByTitle("Java (programming language)");
-        MyListsPageObject.waitForArticleToAppearByTitle("JavaScript");
-
-   }
-
+    }
 }
